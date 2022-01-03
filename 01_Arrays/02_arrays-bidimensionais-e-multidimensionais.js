8f7d377f499ae82e45ff8d3e3b6db766d1f0d08a
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-01-03 17:36:04 -0300

    Inseri mais exercícios sobre arrays

---
+++ 01_Arrays/02_arrays-bidimensionais-e-multidimensionais.js
@@ -0,0 +1,16 @@
+// Array unidimensional
+
+let arrayExample = [0, 1, 2, 3, 4, 5];
+
+// Array bidimensional
+
+let arrayBiExample = [[1 , 2], [2, 3], [3, 4], [4, 5]];
+
+// Array multidimensional
+
+let arrayMultiExample = [
+  [[1 , 2], [2, 3], [3, 4], [4, 5]],
+  [[1 , 2], [2, 3], [3, 4], [4, 5]],
+  [[1 , 2], [2, 3], [3, 4], [4, 5]],
+  [[1 , 2], [2, 3], [3, 4], [4, 5]]
+];
