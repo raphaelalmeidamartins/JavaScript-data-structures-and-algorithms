714d1f4d82c8eb133d886f343d491f5f40fb5e13
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-01-03 17:31:46 -0300

    Renomeei arquivo

---
+++ 01_Arrays/01_inserir-e-remover-elementos.js
@@ -0,0 +1,17 @@
+let arrayExample = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// Inserindo elementos no final do array
+arrayExample.push(10, 11, 12, 13);
+console.log(arrayExample);
+
+// Inserindo elementos no ínício do array
+arrayExample.unshift(-1);
+console.log(arrayExample);
+
+// Removendo elementos do final do array
+arrayExample.pop();
+console.log(arrayExample);
+
+// Removendo elementos do inicio do array
+arrayExample.shift();
+console.log(arrayExample);
