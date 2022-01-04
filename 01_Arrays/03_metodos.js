338fdc7ddb30b6e03b4eb1e9d7b9ec5597a854b9
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-01-04 15:54:52 -0300

    Exercício dos métodos map e filter

--- 01_Arrays/03_metodos.js
+++ 01_Arrays/03_metodos.js
@@ -6,13 +6,19 @@ console.log(negativeNumbers.concat(0, positiveNumbers));
 // every
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-const isEven = x => x % 2 === 0; // arrow function
+const isEven = (x) => x % 2 === 0; // arrow function
 numbers.every(isEven); // encerra as iterações no primeiro número que retornar false
 
 // some
 numbers.some(isEven); // lógica oposta do every
 
 // forEach
-numbers.forEach(x => console.log(x % 2 === 0)); // percorre o array  inteiro sem interromper as iterações
+numbers.forEach((x) => console.log(x % 2 === 0)); // percorre o array  inteiro sem interromper as iterações
 
-// map
+// map e filter
+const myMap = numbers.map(isEven); // cria outro array com o retorno da função isEven para cada elemento do array original
+const myFilter = numbers.filter(isEven); // cria outro array com cada número do array original que a função isEven retorna true;
+console.log(myMap);
+console.log(myFilter);
+
+
